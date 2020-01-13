3fe95cf2a862dbf81919128a36023cc4910a3e05
chrisChen <lei110408@gmail.com>
2020-01-12 21:18:10 -0800

    edit basic layout for showsing which piece user should click

--- frontend/src/layouts/index.js
+++ frontend/src/layouts/index.js
@@ -1,12 +1,27 @@
 import styles from './index.css';
+import {connect} from "dva";
+import Spinner from "react-spinkit";
+import React from 'react'
+
 
 function BasicLayout(props) {
+
+  const {checker,gettingGameData} =props
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-      {props.children}
+      { gettingGameData?<Spinner
+        name='ball-beat'
+        color='olive'
+        style={{ textAlign: "center" }}
+      />:<>
+        <h1 className={styles.title}>{checker.isRedRound?'Click Red Piece':'Click Black Piece'}</h1>
+        {props.children}
+      </>}
     </div>
   );
 }
 
-export default BasicLayout;
+export default connect(({checker,loading}) => ({
+  checker: checker,
+  gettingGameData: loading.effects['checker/setGameData'],
+}))(BasicLayout)
